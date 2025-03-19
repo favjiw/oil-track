2f5125a9829867711758d652b061586640c12d39
favjiw <mfavianj@gmail.com>
2025-03-19 14:31:22 +0700

    add color

---
+++ lib/app/modules/shared/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color blue50 = const Color(0xFFE6ECFF);
+Color blue100 = const Color(0xFFCCD9FF);
+Color blue200 = const Color(0xFF99B3FF);
+Color blue300 = const Color(0xFF678DFE);
+Color blue400 = const Color(0xFF3466FE);
+Color blue500 = const Color(0xFF0140FD);
+Color blue600 = const Color(0xFF0133CB);
+Color blue700 = const Color(0xFF012798);
+Color blue800 = const Color(0xFF001A66);
+Color blue900 = const Color(0xFF000D33);
+Color blue950 = const Color(0xFF000619);
+Color blueGrad1 = const Color(0xFF013FFC);
+Color blueGrad2 = const Color(0xFF012696);
